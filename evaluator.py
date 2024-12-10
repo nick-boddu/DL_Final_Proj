6c4126c2c48b141afa3c70288a7801e61f12feba
nick-boddu <nb3968@nyu.edu>
2024-12-09 19:09:05 -0500

    Small fix again

--- evaluator.py
+++ evaluator.py
@@ -112,7 +112,7 @@ class ProbingEvaluator:
             for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
                 # TODO: Forward pass through your model
-                pred_encs, _ = model(states=batch.states, actions=batch.actions)
+                pred_encs, _ = model(batch.states, batch.actions)
                 pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
                 # Make sure pred_encs has shape (T, BS, D) at this point
